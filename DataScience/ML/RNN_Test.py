e085f00de347e293ee95ce28b9db6727f983ca1f
rkelly3204 <39748461+rkelly3204@users.noreply.github.com>
2019-10-17 16:32:04 -0700

    Create RNN_Test.py

---
+++ DataScience/ML/RNN_Test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 13:02:38 2019
+
+@author: Ryan_Kelly
+"""
+
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+
+#SETUP
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rnn"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+#--------------------------------------------------------------
+## BASIC RNN's
+#Manual RNN
+reset_graph()
+
+n_inputs = 3
+n_neurons = 5
+
+X0 = tf.placeholder(tf.float32, [None, n_inputs])
+X1 = tf.placeholder(tf.float32, [None, n_inputs])
+
+Wx = tf.Variable(tf.random_normal(shape=[n_inputs, n_neurons],dtype=tf.float32))
+Wy = tf.Variable(tf.random_normal(shape=[n_neurons,n_neurons],dtype=tf.float32))
+b = tf.Variable(tf.zeros([1, n_neurons], dtype=tf.float32))
+
+Y0 = tf.tanh(tf.matmul(X0, Wx) + b)
+Y1 = tf.tanh(tf.matmul(Y0, Wy) + tf.matmul(X1, Wx) + b)
+
+init = tf.global_variables_initializer()
+
+# Mini Batch        instance 0, instance 1, instance 2, instance 3
+X0_batch = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 0, 1]]) # t = 0
+X1_batch = np.array([[9, 8, 7], [0, 0, 0], [6, 5, 4], [3, 2, 1]]) # t = 1
+
+with tf.Session() as sess:
+    init.run()
+    Y0_val, Y1_val = sess.run([Y0, Y1], feed_dict={X0: X0_batch, X1: X1_batch})
+# Output
+print(Y0_val) #output at t = 0
+print(Y1_val) #output at t = 1
