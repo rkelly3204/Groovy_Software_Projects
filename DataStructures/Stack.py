4ec419b68674970e78309a51d796b6639946cd4b
rkelly3204 <39748461+rkelly3204@users.noreply.github.com>
2019-10-17 16:48:43 -0700

    Create Stack.py

---
+++ DataStructures/Stack.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class Stack:
+    
+    def __init__(self, iteml=[]):
+        self.items = iteml
+
+    def isEmpty(self):
+        if self.items == []:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.items[-1:][0]
+
+    def pop(self):
+        return self.items.pop()
+
+    def push(self, item):
+        self.items.append(item)
+        return 0
