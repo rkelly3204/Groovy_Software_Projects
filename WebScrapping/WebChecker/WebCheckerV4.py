2b39bf1991546b13de68dc3a87f62c5d1179ab3a
Ryan Kelly <39748461+rkelly3204@users.noreply.github.com>
2020-05-23 12:50:39 -0700

    Update WebCheckerV4.py

--- WebScrapping/WebChecker/WebCheckerV4.py
+++ WebScrapping/WebChecker/WebCheckerV4.py
@@ -6,6 +6,7 @@ from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless")
@@ -17,7 +18,7 @@ options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(),options=options)
 
 logging.basicConfig(filename = 'WebCheckerV3.log',
                     level= logging.INFO,
