a25d442cf9b990dc786d45b8bb19ec6029733222
Ryan Kelly <39748461+rkelly3204@users.noreply.github.com>
2020-05-24 16:43:20 -0700

    Create download_url.go

---
+++ GO/WebScraping/download_url.go
@@ -0,0 +1,31 @@
+// Taken from https://www.devdungeon.com/content/web-scraping-go
+package main
+
+import (
+    "io"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    // Make request
+    response, err := http.Get("https://www.devdungeon.com/archive")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer response.Body.Close()
+
+    // Create output file
+    outFile, err := os.Create("output.html")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer outFile.Close()
+
+    // Copy data from HTTP response to file
+    _, err = io.Copy(outFile, response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
