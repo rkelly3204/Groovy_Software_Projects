4dff40461340cffd78862ae20624e8ef79b23f29
Ryan Kelly <39748461+rkelly3204@users.noreply.github.com>
2020-05-25 11:14:19 -0700

    Create MakeDynamodb.py

---
+++ AWS/Dynamodb/MakeDynamodb.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.create_table(
+        TableName = 'Movies',
+        KeySchema = [
+            {
+                'AttributeName': 'year',
+                'KeyType' : 'HASH' # Partion Key
+            },
+
+            {
+                'AttributeName': 'title',
+                'KeyType': 'RANGE' # Sort key
+            }
+        ],
+
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'year',
+                'AttributeType': 'N'
+            },
+
+            {
+                'AttributeName': 'title',
+                'AttributeType': 'S'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+
+print('Table status:', table.table_status)
+
+print('Waiting for', table.name, 'to complete creating...')
+table.meta.client.get_waiter('table_exists').wait(TableName='Movies')
+print('Table status:', dynamodb.Table('Movies').table_status)
